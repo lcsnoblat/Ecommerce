2b7fb7927308439b5c8100bb959ade7ec3632703
Lucas Noblat <lucas.almeida@al.infnet.edu.br>
2017-12-20 19:50:12 -0200

    Correção

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'core/static')
-STATIC_URL = 'core/static/'
+STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (

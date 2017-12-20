bb76985c3b8315934a1e409b12b82631fcee6823
Lucas Noblat <lucas.almeida@al.infnet.edu.br>
2017-12-20 18:27:47 -0200

    removendo comentários

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -16,9 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '-uhpfb-=5&l!s0h5sn8*(3a^slvqng24z7b$0yt!a8w+2*$m9#'
 
